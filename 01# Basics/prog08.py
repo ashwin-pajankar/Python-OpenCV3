b5c3be4b1bbe38b56cb2e865f33f6f38dad1fea9
ASHWIN PAJANKAR <ashwin-pajankar@users.noreply.github.com>
2017-11-05 08:13:00 +0530

    Added some more code for OpenCV

--- 01# Basics/prog08.py
+++ 01# Basics/prog08.py
@@ -6,10 +6,16 @@ This is a temporary script file.
 """
 
 import cv2
+#import numpy as np
 
 def main():
-    pass
-
+    
+    imgpath = "D:\\Youtube Code\\Python\\Python OpenCV3\\Python-OpenCV3\\Dataset\\4.2.04.tiff"
+    img1 = cv2.imread(imgpath, 1)
+    
+    cv2.imshow('Lena', img1)
+    cv2.waitKey(0)
+    cv2.destroyWindow('Lena')
     
 if __name__ == "__main__":
     main()
